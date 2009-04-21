5312097d6ba503df19fec48c0bb349e12cdff0c7
Dean Hall <dwhall256@gmail.com>
2009-04-21 02:59:18 +0000

    issue #27 mainlined directly

--- src/tools/pmDist.py
+++ src/tools/pmDist.py
@@ -17,17 +17,27 @@ and creates release tag in the svn repository
 import os, string, subprocess, sys, tempfile
 
 
-REPOS = "https://svn.pymite.python-hosting.com/"
-
+REPOS = "https://python-on-a-chip.googlecode.com/svn/"
 
 RELEASE_NUM = sys.argv[1]
 assert string.atoi(RELEASE_NUM, 16) > 6
 print "Building package for release", RELEASE_NUM, "..."
 PM_RELEASE = "pymite-%s" % RELEASE_NUM
 
-# Make a fresh export (no .svn folders) in a temporary directory
+# Tag the release in the repository
+subprocess.check_call(["svn",
+                       "cp",
+                       REPOS + "trunk",
+                       REPOS + "tags/%s" % PM_RELEASE,
+                       '-m "Tagging PyMite release %s"' % RELEASE_NUM],
+                      cwd = tempdir)
+
+# Export (no .svn folders) the tagged release in a temporary directory
 tempdir = tempfile.gettempdir()
-subprocess.check_call(["svn", "export", REPOS + "trunk", PM_RELEASE],
+subprocess.check_call(["svn", 
+                       "export", 
+                       REPOS + "tags/%s" % PM_RELEASE, 
+                       PM_RELEASE],
                       cwd = tempdir)
 
 # Build html docs
@@ -39,13 +49,5 @@ subprocess.check_call(["tar -cz %s/ > %s.tar.gz" % (PM_RELEASE, PM_RELEASE)],
                       cwd = tempdir, shell=True)
 subprocess.check_call(["cp %s.tar.gz ." % os.path.join(tempdir, PM_RELEASE)],
                       shell=True)
-
-# Create release tag in svn repository
-subprocess.check_call(["svn",
-                       "cp",
-                       REPOS + "trunk",
-                       REPOS + "tags/%s" % PM_RELEASE,
-                       '-m "Tagging PyMite release %s"' % RELEASE_NUM],
-                      cwd = tempdir)
 
 print "Done."
