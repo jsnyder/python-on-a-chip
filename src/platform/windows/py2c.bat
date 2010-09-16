bb10c1230af5ebb34d939f80d515f08836adfba7
Dean Hall <dwhall256@gmail.com>
2010-09-16 17:56:49 +0000

    Issue #137: mainlined directly

--- src/platform/windows/py2c.bat
+++ src/platform/windows/py2c.bat
@@ -7,7 +7,7 @@ rem
 rem Assumes python.exe is on the path
 
 rem build the 'system' libraries
-python ../../tools/pmImgCreator.py -c -s -o ../../vm/pmstdlib_img.c --native-file=../../vm/pmstdlib_nat.c ../../lib/ipm.py ../../lib/list.py ../../lib/dict.py ../../lib/__bi.py ../../lib/sys.py ../../lib/string.py
+python ../../tools/pmImgCreator.py -f pmfeatures.py -c -s -o ../../vm/pmstdlib_img.c --native-file=../../vm/pmstdlib_nat.c ../../lib/ipm.py ../../lib/list.py ../../lib/dict.py ../../lib/__bi.py ../../lib/sys.py ../../lib/string.py
 
 rem build the 'local' libraries
-python ../../tools/pmImgCreator.py -c -u -o main_img.c --native-file=main_nat.c main.py 
+python ../../tools/pmImgCreator.py -f pmfeatures.py -c -u -o main_img.c --native-file=main_nat.c main.py 
