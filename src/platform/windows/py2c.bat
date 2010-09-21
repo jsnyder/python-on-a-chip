f2fdea8ebe2da71ada09fea39bb08db6e254fcab
Bryan A. Jones <bjones@ece.msstate.edu>
2010-09-21 22:14:00 +0000

    Issue #141 mainlined directly

--- src/platform/windows/py2c.bat
+++ src/platform/windows/py2c.bat
@@ -1,13 +1,14 @@
-@echo off
-
-rem
-rem Simple batch file to generate the needed .c files
-rem for building with an IDE like VS or Keil
-rem
-rem Assumes python.exe is on the path
-
-rem build the 'system' libraries
+@rem
+@rem Simple batch file to generate the needed .c files
+@rem for building with an IDE like VS or Keil
+@rem
+@rem Assumes python.exe is on the path
+@
+@rem build the 'system' libraries
 python ../../tools/pmImgCreator.py -f pmfeatures.py -c -s -o ../../vm/pmstdlib_img.c --native-file=../../vm/pmstdlib_nat.c ../../lib/ipm.py ../../lib/list.py ../../lib/dict.py ../../lib/__bi.py ../../lib/sys.py ../../lib/string.py
-
-rem build the 'local' libraries
+@
+@rem build the 'local' libraries
 python ../../tools/pmImgCreator.py -f pmfeatures.py -c -u -o main_img.c --native-file=main_nat.c main.py 
+@
+@rem build the pmfeatures.h file
+python ../../tools/pmGenPmFeatures.py pmfeatures.py > pmfeatures.h
